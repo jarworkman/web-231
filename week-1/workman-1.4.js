56c935d69094baadb7cf42061e2845741143d6cc
Jarred Workman <jarworkman@gmail.com>
2019-05-29 22:47:29 -0500

    added exercise 1.4

---
+++ week-1/workman-1.4.js
@@ -0,0 +1,24 @@
+/*
+============================================
+; Title: Assignment 1.4 - Primitive Types
+; Author: Jarred Workman
+; Date: 29 May 2019
+; Modified By:
+; Description: This program demonstrates the
+; use of JavaScript types, values, and
+; and variables.  Will log the output to
+: the console.
+; 
+;===========================================
+*/ 
+
+var firstName = 'Jarred';
+var lastName = 'Workman';
+var age = '23';
+var street = '111 1st St';
+var city = 'New York';
+var zipCode = '123456';
+
+console.log('Name: ' + firstName + ' ' + lastName);
+console.log('Age: ' + age);
+console.log('Address: ' + street + ' ' + city + ' ' + zipCode);
