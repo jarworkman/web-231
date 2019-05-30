9d4ca4c88d481c32e4cee9510de16e9b378a4a11
Jarred Workman <jarworkman@gmail.com>
2019-05-30 17:50:09 -0500

    added 1.1 fix my script

---
+++ week-1/fix-me-1.1.js
@@ -0,0 +1,27 @@
+/*
+============================================
+; Title: Assignment: Discussion 1 - Values and Variables
+; Author: Jarred Workman
+; Date: 29 May 2019
+; Modified By:
+; Description: This program demonstrates the
+; use of JavaScript types, values, and
+; and variables in an application.
+:
+; This program has 2 errors.
+; Please note: Time zone may be different depending on your location
+;===========================================
+*/ 
+
+/*
+    Expected output:
+    Write to console,
+    Name: Jarred Workman
+    Date: Sun May 26 2019 12:00:00 GMT -0500 (Central Daylight Time)
+*/
+
+let firstName = 'Jarred';
+let lastName = 'Workman';
+let todaysDate = new Date('May 26, 2019 12:00:00');
+
+console.log('Name: ' + firstName + '\n' + lastName + '\n' + todaysDate);
