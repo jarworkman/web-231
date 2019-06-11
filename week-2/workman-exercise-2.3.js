da33c36aaf1b24e7869dd8fb305ea728128b02f8
Jarred Workman <jarworkman@gmail.com>
2019-06-10 19:53:04 -0500

    added exercise 2.3

---
+++ week-2/workman-exercise-2.3.js
@@ -0,0 +1,21 @@
+/*
+============================================
+; Title:  workman-exercise-2.3.js
+; Author: Jarred Workman 
+; Date:   10 June 2019
+; Description: Shows the usability of functions
+; in vanilla JS
+;===========================================
+*/
+
+//declare function property
+myName.jarred = 'Jarred';
+
+//Function declaration
+function myName() {
+    return myName.jarred;
+}
+
+//log the output
+console.log(myName());
+
--- week-2/workman-header-test.js
+++ week-2/workman-header-test.js
@@ -1,3 +1,11 @@
+/*
+============================================
+; Title:  workman-header-test.js
+; Author: Jarred Workman 
+; Date:   10 June 2019
+; Description: Unit test for header.js
+;===========================================
+*/
 
 //declaring and importing the header
 const header = require('./workman-header');
