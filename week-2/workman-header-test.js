3cfd0a03fa40aea91170e88b4689bb469543fe72
Jarred Workman <jarworkman@gmail.com>
2019-06-10 19:25:51 -0500

    Added header and header-test

---
+++ week-2/workman-header-test.js
@@ -0,0 +1,6 @@
+
+//declaring and importing the header
+const header = require('./workman-header');
+
+//logging the output
+console.log(header.display('Jarred', 'Workman', 'Header'));
---
+++ week-2/workman-header.js
@@ -0,0 +1,20 @@
+/*
+============================================
+; Title:  workman-header.js
+; Author: Jarred Workman 
+; Date:   10 June 2019
+; Description: Displays a formatted header
+;===========================================
+*/
+
+/**
+ * Params: firstName, lastName, assignment
+ * Response: output
+ * Description: Returns a well-formatted string header
+ */
+exports.display = function (firstName, lastName, assignment) {
+  let output = '\n' + firstName + ' ' + lastName + '\n' + assignment + '\nDate: ' +
+    new Date().toLocaleDateString()
+
+  return output
+}
