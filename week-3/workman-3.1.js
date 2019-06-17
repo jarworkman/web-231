6e28d4797705affe2e642341b6f22894c7288ec1
Jarred Workman <jarworkman@gmail.com>
2019-06-17 17:25:45 -0500

    discussion 3.1

---
+++ week-3/workman-3.1.js
@@ -0,0 +1,23 @@
+/*
+============================================
+; Title:  workman-3.1.js
+; Author: Jarred Workman 
+; Date:   10 June 2019
+; Description: Shows the process of looping
+; with 2 errors to be corrected
+;===========================================
+*/
+
+/* Expected output
+; You're on loop number 1
+; You're on loop number 2
+; You're on loop number 3
+*/
+
+// declaring condition
+let condition = 5
+
+//initiate for loop
+for (i = 0; i < condition; i++) {
+    console.log("You're on loop number " + i);
+}
