00836352e1e30c74969d5ce9801901ed2adaac34
Jarred Workman <jarworkman@gmail.com>
2019-06-20 19:20:45 -0500

    created array problem

---
+++ week-4/fix-me-week-4.js
@@ -0,0 +1,32 @@
+/*
+============================================
+; Title:  fix-me.js
+; Author: Jarred Workman 
+; Date:   20 June 2019
+; Description: Demonstrates the use of arrays
+: has 2 errors to be fixed
+;===========================================
+*/
+
+//Displaying header
+const header = require("../week-2/workman-header");
+console.log(
+  header.display(
+    "Jarred",
+    "Workman",
+    "Discussion 4.1 - Arrays"
+  )
+);
+//instantiate array
+var number = [0,1,2,3,4,5];
+
+//log a single value of the array
+console.log('I really love the number 3, check it out! ' + number[2] + '\n');
+
+//declare function to log
+function logIt(item) {
+  console.log(item);
+}
+
+//log all values of the array
+number.forEach(logIt);
