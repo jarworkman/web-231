5d33b30d84c0d6646a8a88105fa2d5e79a1b3e9b
Jarred Workman <jarworkman@gmail.com>
2019-06-24 23:09:40 -0500

    finished 4.3

---
+++ week-4/workman-4.3.js
@@ -0,0 +1,28 @@
+/*
+============================================
+; Title:  workman-4.3.js
+; Author: Jarred Workman 
+; Date:   23 June 2019
+; Description: Using an array to print out
+; a vehicle name based on a matching string
+;===========================================
+*/
+
+//Displaying header
+const header = require("../week-2/workman-header");
+console.log(header.display("Jarred", "Workman", "Exercise 4.2 - Arrays"));
+
+//declare vehicle array
+let vehicles = ['Car', 'Boat', 'Plane', 'Ship', 'Helicopter'];
+
+//declare function to match array to string
+const getValue = (vehicles, matcher) => {
+    for (x of vehicles) {
+        if (matcher === x) {
+            console.log(x);
+        };
+    };
+};
+
+//call function with vehicle string
+getValue(vehicles, 'Plane');
